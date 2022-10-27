5164120be4551ddd601aa5b6c93c95fa8831f190
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-27 12:38:40 -0500

    reference.py

--- reference.py
+++ reference.py
@@ -1,4 +1,4 @@
-# will use flask_social_media_backend as reference to create my flask backend..
+# will use flask_social_media_backend as reference to create my stock app flask backend..
 # copy content and paste it, once I am confident enough, try to do some routes/models on my own..
 # created two separate blueprints, one for authentication/user db, the other for the user's ability to create a portfolio....
 
