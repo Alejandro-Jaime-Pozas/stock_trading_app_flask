e4ce0540f0f80ce0a3129a9c777eb05b281c0cef
Alex Jaime <ajaimepozas@gmail.com>
2023-08-04 09:22:55 -0600

    comments

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__)) # COME BACK
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # COME BACK
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess' # gets key from the running app environment and if None, returns sarcastic 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db') # if there is an env variable with key 'DATABASE_URL', set to that, if not sqlite
     SQLALCHEMY_TRACK_MODIFICATIONS = False # COME BACK
