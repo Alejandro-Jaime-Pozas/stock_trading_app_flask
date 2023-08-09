867fef84288f984042db1c0a34229fd50450ccd8
Alex Jaime <ajaimepozas@gmail.com>
2023-08-09 09:32:17 -0600

    sogs

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-# this should not appear in the master branch until committed. 
+# # this should not appear in the master branch until committed. 
 
-def sum_numbers(*args):
-    total = 0
-    for num in args:
-        total += num
-    return total
+# def sum_numbers(*args):
+#     total = 0
+#     for num in args:
+#         total += num
+#     return total
 
-result = sum_numbers(1, 2, 3, 4, 5)
-print(result)  # Output: 15
+# result = sum_numbers(1, 2, 3, 4, 5)
+# print(result)  # Output: 15
+
+data = {'hair':'black', 'size':'large'}
+
+def dog(**kwargs):
+    return kwargs
+
+
+# d = dog(hair='black', size='large')
+d = dog(**data)
+print(d)
