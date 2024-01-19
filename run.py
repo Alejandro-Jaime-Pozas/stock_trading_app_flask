45dce115fb9b8b8f2746d4485c85b38c1d64f209
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-19 09:22:11 -0600

    trying to make flask shell run wo importing models

--- run.py
+++ run.py
@@ -2,16 +2,32 @@
 
 from app import app, db
 from app.blueprints.auth.models import User
-# from app.blueprints.portfolio.models import Post
-
-if __name__ == '__main__':
-    app.run()
+from app.blueprints.portfolio.models import Stock
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {
+        'app': app,
         'db': db,
         'User': User,
-        # 'Post': Post
+        'Stock': Stock
     }
+
+
+if __name__ == '__main__':
+    import sys
+
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'shell':
+        # Run Flask shell if 'shell' argument is provided
+        from flask.globals import _app_ctx_stack
+        with app.app_context():
+            ctx = _app_ctx_stack.top
+            ctx.app = app
+            ctx.push()
+            import code
+            code.interact(local=make_shell_context())
+    else:
+        # Otherwise, run the development server
+        app.run()
